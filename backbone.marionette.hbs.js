0f15e93b578bcd37d6f1dbd1968d7f7c51202929
Sven Lito <me@svenlito.com>
2013-06-12 15:54:51 +0100

    [feature] adds write method

--- backbone.marionette.hbs.js
+++ backbone.marionette.hbs.js
@@ -2,21 +2,53 @@
 // it loads and compiles Handlebars templates
 
 define([
+  'text',
   'handlebars'
 ],
 
-function (Handlebars) {
+function (text, Handlebars) {
 
   'use strict';
 
-  var loadResource = function (resourceName, parentRequire, callback, config) {
-    parentRequire([("text!" + resourceName + ".html")], function (templateContent) {
-      callback(Handlebars.compile(templateContent));
+  var buildCache = {},
+      buildCompileTemplate = 'define("{{pluginName}}!{{moduleName}}", ["handlebars"], function(handlebars) {return handlebars.template({{{fn}}})});',
+      buildTemplate;
+
+  var load = function (moduleName, parentRequire, load, config) {
+
+    text.get(parentRequire.toUrl(moduleName + '.html'), function(data) {
+
+      if (config.isBuild) {
+        buildCache[moduleName] = data;
+        load();
+      } else {
+        load(Handlebars.compile(data));
+      }
+
     });
+
+  };
+
+  var write = function (pluginName, moduleName, write) {
+
+    if (moduleName in buildCache) {
+
+      if (!buildTemplate) {
+          buildTemplate = Handlebars.compile(buildCompileTemplate);
+      }
+
+      write(buildTemplate({
+        pluginName: pluginName,
+        moduleName: moduleName,
+        fn: Handlebars.precompile(buildCache[moduleName])
+      }));
+    }
+
   };
 
   return {
-    load: loadResource
+    load: load,
+    write: write
   };
 
 });
